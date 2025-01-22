7f936ade558434a919322fba021fdfce1334f952
Aravindhan Baskaran <baravindhan0505@gmail.com>
2025-01-22 04:36:43 +0000

    002

---
+++ models/example/EMP_DEP_TBL.sql
@@ -0,0 +1,12 @@
+{{ config(
+    materialized='table'
+    ) 
+}}
+with source_data as (
+
+    SELECT E.NAME EMPLOYEE_NAME, D.NAME DEPARTMENT_NAME FROM EMPLOYEES E
+    JOIN DEPARTMENT D ON E.DEPARTMENT_ID = D.DEPARTMENT_ID
+
+)
+
+select * from source_data
